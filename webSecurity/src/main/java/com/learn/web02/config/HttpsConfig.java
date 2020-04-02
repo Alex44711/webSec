a91d29169c08cc8433181136becacc1bf6dff7c0
Alex44711 <secondsmall86@gmail.com>
2020-04-02 13:57:32 -0400

    update default port

--- webSecurity/src/main/java/com/learn/web02/config/HttpsConfig.java
+++ webSecurity/src/main/java/com/learn/web02/config/HttpsConfig.java
@@ -32,7 +32,7 @@ public class HttpsConfig {
     public Connector httpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(80);
         connector.setSecure(false);
         connector.setRedirectPort(443);
         return connector;

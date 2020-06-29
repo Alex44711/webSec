02d642fd9127d1c92908feb967028a4fe8229f98
Alex44711 <secondsmall86@gmail.com>
2020-06-28 22:26:12 -0400

    update jenkins build file

--- webSecurity/build.sh
+++ webSecurity/build.sh
@@ -1,6 +1,8 @@
 cd /var/lib/jenkins/workspace/webSec
 mvn package
 mv /var/lib/jenkins/workspace/webSec/webSecurity/target/webSecurity01-1.0.jar /home/ec2-user/webSecurity01-1.0.jar
+rm -rf *
 cd /home/ec2-user
 cp /dev/null temp.txt
-nohup java -jar webSecurity01-1.0.jar > temp.txt 2>&1 &
+ps -ef | pgrep -f webSecurity01-1.0.jar | xargs kill -s 9
+nohup java -jar webSecurity01-1.0.jar >temp.txt &

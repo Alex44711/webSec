35c5c0778d6485a7f0f9ec408160c8f572650753
Alex44711 <secondsmall86@gmail.com>
2020-06-29 00:01:40 -0400

    Update build file

--- webSecurity/build.sh
+++ webSecurity/build.sh
@@ -6,3 +6,12 @@ cd /home/ec2-user
 cp /dev/null temp.txt
 ps -ef | pgrep -f webSecurity01-1.0.jar | xargs kill -s 9
 nohup java -jar webSecurity01-1.0.jar >temp.txt &
+sleep 20s
+echo "Start Status Judge"
+cat temp.txt | while read line
+do
+    if [[ $line == *"Started Application"* ]]
+    then
+        echo "*******Start Success*******"
+    fi
+done

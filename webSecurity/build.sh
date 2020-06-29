d44f38bbe5042569d4221a49deff2f6c9fd54a09
Alex44711 <secondsmall86@gmail.com>
2020-06-29 00:36:22 -0400

    update build file

--- webSecurity/build.sh
+++ webSecurity/build.sh
@@ -5,7 +5,7 @@ rm -rf *
 cd /home/ec2-user
 cp /dev/null temp.txt
 ps -ef | pgrep -f webSecurity01-1.0.jar | xargs kill -s 9
-nohup java -jar webSecurity01-1.0.jar >temp.txt &
+BUILD_ID=dontKillMe nohup java -jar webSecurity01-1.0.jar >temp.txt &
 sleep 20s
 echo "Start Status Judge"
 cat temp.txt | while read line

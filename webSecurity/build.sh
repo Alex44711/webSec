aa88e5b4f0d4bb9fe90603f8d7f1a3b307025550
Alex44711 <secondsmall86@gmail.com>
2020-07-20 11:27:00 -0400

    better jekins console log

--- webSecurity/build.sh
+++ webSecurity/build.sh
@@ -1,3 +1,13 @@
+buildStatus(){
+    cat temp.txt | while read line
+    do
+        if [[ $line == *"Started Application"* ]]
+        then
+            echo "*******Start Success*******"
+        fi
+    done
+}
+
 cd /var/lib/jenkins/workspace/webSecBlog
 mvn package
 mv /var/lib/jenkins/workspace/webSecBlog/webSecurity/target/webSecurity01-1.0.jar /home/ec2-user/webSecurity01-1.0.jar
@@ -8,10 +18,4 @@ ps -ef | pgrep -f webSecurity01-1.0.jar | xargs kill -s 9
 BUILD_ID=dontKillMe nohup java -jar webSecurity01-1.0.jar >temp.txt &
 sleep 20s
 echo "Start Status Judge"
-cat temp.txt | while read line
-do
-    if [[ $line == *"Started Application"* ]]
-    then
-        echo "*******Start Success*******"
-    fi
-done
+buildStatus
